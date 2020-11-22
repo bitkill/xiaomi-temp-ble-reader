c348b3731c26905356e1d3834a854c0c76fba3f4
Rui Fernandes <1.ruifn@gmail.com>
2020-11-22 03:08:57 +0000

    Remove run shell script

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-for file in "mqtt" "devices" "averages"; do
-  [ -f /config/${file}.ini ] && cp -f /config/${file}.ini /usr/src/app/
-done
-exec "/usr/bin/python3" "/usr/src/app/data-read.py"
